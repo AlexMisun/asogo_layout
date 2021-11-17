222fb4891d814d069f26d3015b80827090b58a77
luckyone <luckyone1221@gmail.com>
2021-11-17 23:19:14 +0200

    -m made main

--- readme.md
+++ readme.md
@@ -1,3 +1,4 @@
 <!-- -->
 
 1. <https://luckyone1221.github.io/0098ACOGO/public/index.html>
+1. <https://luckyone1221.github.io/0098ACOGO/public/01.html>
--- sourse/js/common.js
+++ sourse/js/common.js
@@ -320,6 +320,56 @@ function eventHandler() {
 		...defaultSl,
 		...freeMomentum,
 	});
+	//
+	let targetSlider = new Swiper('.target-slider-js', {
+		slidesPerView: 'auto',
+		spaceBetween: 16,
+	});
+
+	$('.split-js').each(function (){
+		let h1 = this.querySelector('h1');
+
+		let inner = h1.innerHTML.split(' ');
+		let resulut = [];
+		for (let word of inner){
+			word = `<span>${word}</span>`;
+			resulut.push(word);
+		}
+
+		h1.innerHTML = resulut.join(' ');
+	});
+
+	//img svg
+	$('img.img-svg-js').each(function () {
+		var $img = $(this);
+		var imgClass = $img.attr('class');
+		var imgURL = $img.attr('src');
+		$.get(imgURL, function (data) {
+			// Get the SVG tag, ignore the rest
+			var $svg = $(data).find('svg'); // Add replaced image's classes to the new SVG
+
+			if (typeof imgClass !== 'undefined') {
+				$svg = $svg.attr('class', imgClass + ' replaced-svg');
+			} // Remove any invalid XML tags as per http://validator.w3.org
+
+
+			$svg = $svg.removeAttr('xmlns:a'); // Check if the viewport is set, if the viewport is not set the SVG wont't scale.
+
+			if (!$svg.attr('viewBox') && $svg.attr('height') && $svg.attr('width')) {
+				$svg.attr('viewBox', '0 0 ' + $svg.attr('height') + ' ' + $svg.attr('width'));
+			} // Replace image with new SVG
+
+
+			$img.replaceWith($svg);
+		}, 'xml');
+	});
+	//
+	let sNewsSlider = new Swiper('.sNews-slider-js', {
+		slidesPerView: 'auto',
+		spaceBetween: 16,
+	});
+
+
 	// modal window
 
 };
